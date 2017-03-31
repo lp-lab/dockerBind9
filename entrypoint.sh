963ef6633fdd1105d9ad5368b0e107980deacdc0
Luca 'meti' P <meti-@users.noreply.github.com>
2017-03-31 19:35:03 +0200

    Let the user modify cache and data sizes via docker run variables. First test on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 ROOT_PASSWORD=${ROOT_PASSWORD:-password}
+CACHESIZE=${CACHESIZE:-size}
+DATALIMIT=${DATALIMIT:-limit}
 #export UID=$(id -u dnscache)
 #export GID=$(id -g dnscache)
 #export ROOT=/etc/dnscache
@@ -15,6 +17,14 @@ set_root_passwd() {
   echo "root:$ROOT_PASSWORD" | chpasswd
 }
 
+update_cachesize() {
+  echo "$CACHESIZE" > /etc/dnscache/env/CACHESIZE
+}
+
+update_datalimit() {
+  echo "$DATALIMIT" > /etc/dnscache/env/DATALIMIT
+}
+
 # Set root password from the commandline
 set_root_passwd
 
@@ -23,6 +33,14 @@ set_root_passwd
 
 # Start mini_httpd to serve acf on https
 /usr/sbin/mini_httpd -C /etc/mini_httpd/mini_httpd.conf
+
+if [ -n "${CACHESIZE}" ]; then
+	update_cachesize
+fi
+
+if [ -n "${DATALIMIT}" ]; then
+	update_datalimit
+fi
 
   echo "Starting dnscache..."
   exec /etc/dnscache/run
