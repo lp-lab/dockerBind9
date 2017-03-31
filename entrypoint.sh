73e95d7833fd49469c3c8b5d9fd9de7ded77bba9
Luca 'meti' P <meti-@users.noreply.github.com>
2017-03-31 18:19:02 +0200

    Modified entrypoint.sh to run dnscache start script instead of the binary

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 ROOT_PASSWORD=${ROOT_PASSWORD:-password}
-export UID=$(id -u dnscache)
-export GID=$(id -g dnscache)
-export ROOT=/etc/dnscache
-export IP=0.0.0.0
-export IPSEND=0.0.0.0
-export CACHESIZE=1000000
-DAEMON=/usr/bin/dnscache
-PIDFILE=/var/run/dnscache.pid
+#export UID=$(id -u dnscache)
+#export GID=$(id -g dnscache)
+#export ROOT=/etc/dnscache
+#export IP=0.0.0.0
+#export IPSEND=0.0.0.0
+#export CACHESIZE=1000000
+#DAEMON=/usr/bin/dnscache
+#PIDFILE=/var/run/dnscache.pid
 
 set_root_passwd() {
   echo "root:$ROOT_PASSWORD" | chpasswd
@@ -25,7 +25,7 @@ set_root_passwd
 /usr/sbin/mini_httpd -C /etc/mini_httpd/mini_httpd.conf
 
   echo "Starting dnscache..."
-  exec /usr/bin/dnscache < /dev/urandom
+  exec /etc/dnscache/run
 else
   exec "$@"
 fi
