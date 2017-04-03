493a8003e037a10cf0fc93a59c2059ef3d268928
Luca 'meti' P <meti-@users.noreply.github.com>
2017-04-03 11:55:21 +0200

    Modified README and start.sh to reflect new CLI options

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-docker run --name bind -d --restart=always --publish 53:53/tcp --publish 53:53/udp --publish 10000:10000/tcp --volume /srv/docker/bind:/data lplab2/dockerbind9:latest
+#!/bin/bash
+
+docker run --name bind -d --restart=always   --publish <ip>:53:53/tcp --publish <ip>:53:53/udp --publish <ip>:443:443/tcp --volume /srv/docker/bind:/data --env ROOT_PASSWORD=<password> --env CACHESIZE=<size> --env DATALIMIT=<limit> lplab/dockerdns:latest
