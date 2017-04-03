cbdc7c72a09686d5452382d19d8dff46355cbfd9
Luca 'meti' P <meti-@users.noreply.github.com>
2017-04-03 14:22:12 +0200

    Styling refinement in README and start.sh, added env option for timezone setting

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run --name bind -d --restart=always   --publish <ip>:53:53/tcp --publish <ip>:53:53/udp --publish <ip>:443:443/tcp --volume /srv/docker/bind:/data --env ROOT_PASSWORD=<password> --env CACHESIZE=<size> --env DATALIMIT=<limit> lplab/dockerdns:latest
+docker run --name bind -d --restart=always --publish <ip>:53:53/tcp \
+  --publish <ip>:53:53/udp \
+  --publish <ip>:443:443/tcp \
+  --volume /etc/localtime:/etc/localtime \
+  --volume /srv/docker/bind:/data \
+  --env ROOT_PASSWORD=<password> \
+  --env CACHESIZE=<size> \
+  --env DATALIMIT=<limit> \
+  --env TIMEZONE="<your/timezone>" \
+  lplab/dockerdns:latest
