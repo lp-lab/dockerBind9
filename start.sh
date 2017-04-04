c1db4db122bf14d23506a0da57a8ca0aaeb72de7
Luca 'meti' P <meti-@users.noreply.github.com>
2017-04-04 23:16:54 +0200

    Changed README and start.sh to reflect the new dns daemon

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-docker run --name bind -d --restart=always --publish <ip>:53:53/tcp \
+docker run --name dnsalpine -d --restart=always --publish <ip>:53:53/tcp \
   --publish <ip>:53:53/udp \
   --publish <ip>:443:443/tcp \
   --volume /etc/localtime:/etc/localtime \
-  --volume /srv/docker/bind:/data \
+  --volume /srv/docker/alpinedns:/data \
   --env ROOT_PASSWORD=<password> \
-  --env CACHESIZE=<size> \
-  --env DATALIMIT=<limit> \
   --env TIMEZONE="<your/timezone>" \
-  lplab/dockerdns:latest
+  lplab/dockerdns:alpine_unbound
